9cb0370f3f142a31ac41450d0c2acc2ed3398c5b
Shyam <sshyamdevkumar12@gmail.com>
2026-02-25 14:58:43 +0530

    Fixed navbar invisible links

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -39,7 +39,7 @@ export const NavBar = () => {
   }, []);
 
   return (
-    <Navbar collapseOnSelect id="nav" expand="md" className="py-3 fixed-top navbar-dark-overlay">
+    <Navbar collapseOnSelect id="nav" expand="md" className="py-3 fixed-top">
       <Container fluid>
         <Navbar.Brand className="d-flex flex-column align-items-center">
 
