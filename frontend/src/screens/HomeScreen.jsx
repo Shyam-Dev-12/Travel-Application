d7ba52a8ee4bf35ac98d1400f5c7c2956ba7086d
Shyam <sshyamdevkumar12@gmail.com>
2026-02-25 12:06:45 +0530

    Fixed navbar invisible links

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -31,22 +31,29 @@ export const HomeScreen = () => {
             <NavBar />
               {/* SEARCH BAR */}
             <div>
-              <div id="home-carousel" style={{ position: "relative"}}>
-                <HomeCarousel />
-                 {/* Dark overlay on carousel (optional; increases text visibility) */}
-                <div style={{ position: "absolute", inset: 0, backgroundColor: "rgba(0,0,0,0.5)", }} />
-                <div className="position-absolute w-100 d-flex flex-column align-items-center"
-                      style={{ top: 0, zIndex: 2,}} >
-                <SearchBar scrollToDestinations={scrollToDestinations} />
-              </div>
-                
-                
-                      {/* Content on top of carousel */}
-                        {/* <div className="position-relative d-flex flex-column align-items-center justify-content-start mt-0 pt-0"
-                                style={{ zIndex: 2,}}
-                          > */}
-               
-              </div>
+              <div id="home-carousel" style={{ position: "relative" }}>
+  <HomeCarousel />
+
+  <div
+    style={{
+      position: "absolute",
+      inset: 0,
+      backgroundColor: "rgba(0,0,0,0.5)",
+      zIndex: 1
+    }}
+  />
+
+  <div
+    className="position-absolute w-100 d-flex flex-column align-items-center"
+    style={{
+      top: 0,
+      zIndex: 2,
+      paddingTop: "80px"
+    }}
+  >
+    <SearchBar scrollToDestinations={scrollToDestinations} />
+  </div>
+</div>
             </div>  
         </Container>
           
